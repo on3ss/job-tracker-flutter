2264c5461152d134787fad5b7478a984fb24aa09
Oness Lyngdoh <auditoree9@gmail.com>
2024-06-14 12:56:13 +0530

    added text to screens

--- lib/screens/jobs/jobs.dart
+++ lib/screens/jobs/jobs.dart
@@ -5,6 +5,8 @@ class JobsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Center(
+      child: Text('Jobs'),
+    );
   }
 }
