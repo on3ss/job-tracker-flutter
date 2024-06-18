91d41623db3bc7e0b07a9bf979b922b7b93dc13e
Oness Lyngdoh <auditoree9@gmail.com>
2024-06-18 12:26:38 +0530

    added appbar to jobs screen

--- lib/screens/jobs/jobs.dart
+++ lib/screens/jobs/jobs.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:job_tracker_fl/widgets/custom_appbar.dart';
 
 class JobsScreen extends StatelessWidget {
   const JobsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Jobs'),
+    return const Scaffold(
+      appBar: CustomAppBar(title: "Jobs"),
+      body: SafeArea(child: Text("List")),
     );
   }
 }
