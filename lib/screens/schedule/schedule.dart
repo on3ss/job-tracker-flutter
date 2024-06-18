5a90a0d73fb85749ee3d257cbd43088925ee39a4
Oness Lyngdoh <auditoree9@gmail.com>
2024-06-18 12:19:15 +0530

    added appbar to schedule screen

--- lib/screens/schedule/schedule.dart
+++ lib/screens/schedule/schedule.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:job_tracker_fl/widgets/custom_appbar.dart';
 
 class ScheduleScreen extends StatelessWidget {
   const ScheduleScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Schedule'),
+    return Scaffold(
+      appBar: CustomAppBar(title: "Schedule"),
+      body: Center(
+        child: Text("Schedule"),
+      ),
     );
   }
 }
