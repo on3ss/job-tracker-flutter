a2d458b8c86f63911f8f67a9551ebf61ccd4bc1d
Oness Lyngdoh <auditoree9@gmail.com>
2024-06-19 16:11:28 +0530

    item card

---
+++ lib/mock/mock_exam.dart
@@ -0,0 +1,81 @@
+import 'package:faker/faker.dart';
+
+class MockExam {
+  final int id;
+  final String title;
+  final String? description;
+  final String organization;
+  final String status;
+  final List<MockExamStep> steps;
+
+  MockExam({
+    this.id = 0,
+    required this.title,
+    required this.description,
+    required this.organization,
+    required this.status,
+    required this.steps,
+  });
+}
+
+class MockExamStep {
+  final int id;
+  final String title;
+  final DateTime dateTime;
+  final String venue;
+  final List<MockSubject> subjects;
+
+  MockExamStep({
+    this.id = 0,
+    required this.title,
+    required this.dateTime,
+    required this.venue,
+    required this.subjects,
+  });
+}
+
+class MockSubject {
+  final int id;
+  final String name;
+
+  MockSubject({required this.id, required this.name});
+}
+
+class MockExamGenerator {
+  static final faker = Faker();
+  static final List<String> statuses = ["in-progress", "done", "cancelled"];
+
+  static List<MockExam> generate() {
+    return List.generate(
+      20,
+      (index) => MockExam(
+        id: index,
+        title: faker.lorem
+            .words(faker.randomGenerator.integer(4, min: 2))
+            .join(' '),
+        description: faker.lorem.sentence(),
+        organization: faker.company.name(),
+        status: statuses[faker.randomGenerator.integer(statuses.length - 1)],
+        steps: generateSteps(),
+      ),
+    );
+  }
+
+  static List<MockExamStep> generateSteps() {
+    return List.generate(
+      5,
+      (index) => MockExamStep(
+        id: index,
+        title: faker.lorem.sentence(),
+        dateTime:
+            faker.date.dateTimeBetween(DateTime(2010, 1, 1), DateTime.now()),
+        venue:
+            "${faker.address.buildingNumber()}, ${faker.address.neighborhood()}, ${faker.address.city()} - ${faker.address.zipCode()}",
+        subjects: List.generate(
+          5,
+          (index) => MockSubject(id: index, name: faker.lorem.word()),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/jobs/jobs.dart
+++ lib/screens/jobs/jobs.dart
@@ -1,15 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:job_tracker_fl/mock/mock_exam.dart';
 import 'package:job_tracker_fl/widgets/custom_appbar.dart';
 
-class JobsScreen extends StatelessWidget {
+class JobsScreen extends HookWidget {
   const JobsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: "Jobs"),
+    List<MockExam> exams = useMemoized(() => MockExamGenerator.generate());
+    return Scaffold(
+      appBar: const CustomAppBar(title: "Jobs"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(HeroIcons.plus),
+      ),
       body: SafeArea(
-        child: Placeholder(),
+        child: ListView.builder(
+          itemCount: exams.length,
+          itemBuilder: (BuildContext context, int index) =>
+              JobListItem(exam: exams[index]),
+        ),
+      ),
+    );
+  }
+}
+
+class JobListItem extends StatelessWidget {
+  const JobListItem({
+    super.key,
+    required this.exam,
+  });
+
+  final MockExam exam;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0),
+      padding: const EdgeInsets.all(8.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary.withOpacity(0.4),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Text(
+                  exam.title,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(fontWeight: FontWeight.w600),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(4.0),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 6.0,
+                  vertical: 2.0,
+                ),
+                child: Text(
+                  exam.status,
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary,
+                        fontWeight: FontWeight.w600,
+                      ),
+                ),
+              )
+            ],
+          ),
+          Text(
+            exam.organization,
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          NextStepCard(exam: exam)
+        ],
+      ),
+    );
+  }
+}
+
+class NextStepCard extends StatelessWidget {
+  const NextStepCard({
+    super.key,
+    required this.exam,
+  });
+
+  final MockExam exam;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      margin: const EdgeInsets.only(top: 8.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary.withOpacity(0.8),
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Text(
+                "Next Step",
+                style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+              ),
+            ],
+          ),
+          Text(
+            exam.steps.first.title,
+            style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+          ),
+        ],
       ),
     );
   }
